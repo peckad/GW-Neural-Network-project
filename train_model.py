1de41769b67ea979bebad17914cd63d6b667e595
peckad <96533338+peckad@users.noreply.github.com>
2022-03-25 20:38:57 +0000

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -8,15 +8,14 @@ from z_score import z_score  #import normalising function
 
 def train_model(model, dataset, epochs, label_name, batch_size, validation_split):
  
-	dataset_norm = z_score(dataset) #normalise the features
-	features = {name:np.array(value) for name, value in dataset_norm.items()}
-	features_label = {name:np.array(value) for name, value in dataset.items()}
-	label = np.array(features_label.pop(label_name))
-
+	dataset_norm = z_score(dataset)  #normalise the dataset
+	features = {name:np.array(value) for name, value in dataset_norm.items()}  #extract the features from the normalied dataset
+	label = np.array(features_label.pop(label_name))  #extract the labels from the non-normalised dataset
+	
+	#train the model
 	history = model.fit(features, label, epochs=epochs, batch_size=batch_size, validation_split=validation_split, shuffle=True)
 
 	epochs = history.epoch
 	hist = pd.DataFrame(history.history)
 
 	return epochs, hist
-train_
